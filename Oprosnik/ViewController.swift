ada804dcea5bc7e42abb47cd49a103c7d4468423
ios_dev <futureiosdeveloper@gmail.com>
2021-06-28 09:44:52 +0300

    Initial Commit

---
+++ Oprosnik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oprosnik
+//
+//  Created by Vladimir Syleimanov on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
